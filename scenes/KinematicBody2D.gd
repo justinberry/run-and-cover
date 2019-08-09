c0d4d688f0a1da8b1297a88f3c3810771257b991
Justin Bieber <justin.berry@caradvice.com.au>
2019-08-09 14:29:42 +0700

    Add movement strategy

--- scenes/KinematicBody2D.gd
+++ scenes/KinematicBody2D.gd
@@ -3,20 +3,155 @@ extends KinematicBody2D
 const MOTION_SPEED = 260 # Pixels/second
 const MOUSE_DEBUG_LABEL = '/root/Main/UI/MarginContainer/VBoxContainer/MouseXY'
 
+class Movement:
+  func is_moving():
+    return
+    
+  func get_motion():
+    return
+
+class UpMovement extends Movement:
+  const _MOTION = Vector2(0, -1)
+  
+  func is_moving():
+    return Input.is_action_pressed('move_up')
+  
+  func get_motion():
+    return _MOTION
+  
+  func get_animation():
+    return 'WalkUp'
+    
+class LeftMovement extends Movement:
+  const _MOTION = Vector2(-1, 0)
+  
+  func is_moving():
+    return Input.is_action_pressed('move_left')
+  
+  func get_motion():
+    return _MOTION
+  
+  func get_animation():
+    return 'WalkLeft'
+    
+class DownMovement extends Movement:
+  const _MOTION = Vector2(0, 1)
+  
+  func is_moving():
+    return Input.is_action_pressed('move_down')
+  
+  func get_motion():
+    return _MOTION
+  
+  func get_animation():
+    return 'WalkDown'
+
+class RightMovement extends Movement:
+  const _MOTION = Vector2(1, 0)
+  
+  func is_moving():
+    return Input.is_action_pressed('move_right')
+  
+  func get_motion():
+    return _MOTION
+  
+  func get_animation():
+    return 'WalkRight'
+    
+class RightUpMovement extends Movement:
+  func is_moving():
+    return (
+      Input.is_action_pressed('move_up')
+      and
+      Input.is_action_pressed('move_right')
+    )
+  
+  func get_motion():
+    return RightMovement._MOTION + UpMovement._MOTION
+  
+  func get_animation():
+    return 'WalkRightUp'
+    
+class LeftUpMovement extends Movement:
+  func is_moving():
+    return (
+      Input.is_action_pressed('move_up')
+      and
+      Input.is_action_pressed('move_left')
+    )
+  
+  func get_motion():
+    return LeftMovement._MOTION + UpMovement._MOTION
+  
+  func get_animation():
+    return 'WalkLeftUp'
+    
+class LeftDownMovement extends Movement:
+  func is_moving():
+    return (
+      Input.is_action_pressed('move_down')
+      and
+      Input.is_action_pressed('move_left')
+    )
+  
+  func get_motion():
+    return LeftMovement._MOTION + DownMovement._MOTION
+  
+  func get_animation():
+    return 'WalkLeftDown'
+
+class RightDownMovement extends Movement:
+  func is_moving():
+    return (
+      Input.is_action_pressed('move_down')
+      and
+      Input.is_action_pressed('move_right')
+    )
+  
+  func get_motion():
+    return RightMovement._MOTION + DownMovement._MOTION
+  
+  func get_animation():
+    return 'WalkRightDown'
+    
+class IdleMovement extends Movement:
+  func is_moving():
+    return false
+    
+  func get_motion():
+    return Vector2(0, 0)
+  
+  func get_animation():
+    return 'Idle'
+  
+var MOVEMENTS = [
+  LeftUpMovement.new(),
+  LeftDownMovement.new(),
+  RightDownMovement.new(),
+  RightUpMovement.new(),
+  UpMovement.new(),
+  LeftMovement.new(),
+  DownMovement.new(),
+  RightMovement.new()
+]
+  
+func _movement_factory():
+  for m in MOVEMENTS:
+    if m.is_moving():
+      return m
+  
+  return IdleMovement.new()
+      
 func _update_movement(delta):
   var motion = Vector2()
+  var walk_anim = get_node('Sprite/AnimationPlayer')
+  
+  var movement = _movement_factory()
 
-  if Input.is_action_pressed("move_up"):
-    motion += Vector2(0, -1)
-  if Input.is_action_pressed("move_down"):
-    motion += Vector2(0, 1)
-  if Input.is_action_pressed("move_left"):
-    motion += Vector2(-1, 0)
-  if Input.is_action_pressed("move_right"):
-    motion += Vector2(1, 0)
+  motion = movement.get_motion()
+  walk_anim.play(movement.get_animation())
 
   motion = motion.normalized() * MOTION_SPEED
-
   move_and_slide(motion)
 
 func _triangle_vectors_around(point):
