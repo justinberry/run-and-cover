e6574e273cbca8e7dde40770b4049c2976c03ced
Justin Bieber <justin.berry@caradvice.com.au>
2019-09-01 15:37:04 +1000

    Playing around with angles and stuff

--- src/player/PlayerBody.gd
+++ src/player/PlayerBody.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 const MOTION_SPEED = 260 # Pixels/second
 const MOUSE_DEBUG_LABEL = '/root/Main/UI/MarginContainer/VBoxContainer/MouseXY'
-    
+
 var MOVEMENTS = [
   preload('movement/LeftUpMovement.gd').new(),
   preload('movement/LeftDownMovement.gd').new(),
@@ -45,15 +45,56 @@ const RED_RETICULE = [
   Color(255, 0, 0), Color(255, 0, 0), Color(255, 0, 0)
 ]
 
+const GREEN_RETICULE = [
+  Color(0, 255, 0)
+]
+
+const NUM_POINTS = 32
+
 func _draw_aim():
   var mouse_position = self.get_local_mouse_position()
   
   var point = PoolVector2Array(_triangle_vectors_around(mouse_position))
-  var color = PoolColorArray(RED_RETICULE) 
+  var color = PoolColorArray(RED_RETICULE)
   draw_primitive(point, color, PoolVector2Array())
   
-  var mouse_direction = mouse_position.normalized() * 100
-  draw_line(get_node('Sprite').position, mouse_direction, Color(255, 0, 0), 2)
+  # draw point at player
+  # get angle of player -> mouse
+  # Draw arc from angle (player -> mouse) - (fov / 2)
+  # and (player -> mouse) + (fov / 2)
+  
+  var points = PoolVector2Array()
+  
+  var nb_points = NUM_POINTS
+  var points_arc = PoolVector2Array()
+  var center = position
+  var radius = 200
+  points_arc.push_back(center)
+  
+  var direction = (get_local_mouse_position() - position).normalized()
+  var angle = Vector2(
+    get_node('Sprite').position.x + cos(direction.angle()) * 100,
+    get_node('Sprite').position.y + sin(direction.angle()) * 100
+  )
+  print('angle is: ', angle)
+  
+  var angle_from = angle.angle_to(
+  var angle_to = 45
+  
+  var colors = PoolColorArray(GREEN_RETICULE)
+
+  for i in range(nb_points + 1):
+    var angle_point = deg2rad(angle_from + i * (angle_to - angle_from) / nb_points - 90)
+    points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
+  draw_polygon(points_arc, colors)
+  update()
+  
+  #var direction = (get_local_mouse_position() - position).normalized()
+  #var mouse_direction  = Vector2(
+  #  get_node('Sprite').position.x + cos(direction.angle()) * 100,
+  #  get_node('Sprite').position.y + sin(direction.angle()) * 100
+  #)
+  #draw_line(get_node('Sprite').position, mouse_direction, Color(255, 0, 0), 2)
   get_node(MOUSE_DEBUG_LABEL).set_text(mouse_position)
 
 func _draw():
