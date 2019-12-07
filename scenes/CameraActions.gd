ab6942a71bbc04f54e5e0ec54850f30211430254
Justin Berry <jtberry1@gmail.com>
2019-12-07 21:14:18 +1100

    Add camera toggle

--- scenes/CameraActions.gd
+++ scenes/CameraActions.gd
@@ -6,9 +6,16 @@ const ZOOM_SPEED = 10
 var target_zoom = 2
 var smooth_zoom = 2
 
+var locked = false
+
+var original_parent
+var manual_camera_focus
+
 func _ready():
   set_process_input(true)
   set_physics_process(true)
+  original_parent = self.get_parent()
+  manual_camera_focus = original_parent.get_parent().get_node('ManualCameraFocus')
 
 func is_within_lower_bound():
   return target_zoom > 0.5
@@ -21,9 +28,28 @@ func _physics_process(delta):
 	if smooth_zoom != target_zoom:
 		set_zoom(Vector2(smooth_zoom, smooth_zoom))
 
-func _input(event):
- if event is InputEventMouseButton:
-   if (event.button_index == BUTTON_WHEEL_UP && is_within_lower_bound()):
+func _handle_zoom(event):
+  if (event.button_index == BUTTON_WHEEL_UP && is_within_lower_bound()):
     target_zoom -= ZOOM_SCALE
-   elif (event.button_index == BUTTON_WHEEL_DOWN && is_within_upper_bound()):
+  elif (event.button_index == BUTTON_WHEEL_DOWN && is_within_upper_bound()):
      target_zoom += ZOOM_SCALE
+
+func _handle_camera_toggle():
+  if locked:
+      self.get_parent().remove_child(self)
+      original_parent.add_child(self)
+      self.set_owner(original_parent)
+  else:
+    original_parent.remove_child(self)
+    manual_camera_focus.set_global_position(original_parent.get_global_position())
+    manual_camera_focus.add_child(self)
+    self.set_owner(manual_camera_focus)
+  locked = !locked
+
+func _input(event):
+  if event is InputEventMouseButton:
+    _handle_zoom(event)
+    
+  if Input.is_action_pressed('camera_lock_toggle'):
+    _handle_camera_toggle()
+    
